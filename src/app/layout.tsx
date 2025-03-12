ba8d0cc6ec233e0809adc835641119221b7e2d5e
heroyooi <heroyooi1018@gmail.com>
2025-03-12 22:14:16 +0900

    프로젝트 세팅 (Next.js + Firebase + GA)

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  description: "Next.js + Firebase + GA 교육 자료",
+};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ko">
+      <head>
+        {/* GA 기본 스크립트 추가 */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){ dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
+      <body>
+        {children}
+        <GoogleAnalytics /> {/* 클라이언트 컴포넌트 추가 */}
+      </body>
+    </html>
+  );
+}

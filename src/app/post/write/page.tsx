f143a2c378bcca4112ac4bd7a5cbae511b9cccf4
heroyooi <heroyooi1018@gmail.com>
2025-04-01 12:25:46 +0900

    vercel 에러 해결

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -1,9 +1,13 @@
-import { AddPostForm } from '@/components/posts';
+import dynamic from 'next/dynamic';
 
 export const metadata = {
   title: '글 작성 | Next.js + Firebase + GA',
   description: 'Next.js + Firebase + GA 글 작성 페이지입니다.',
 };
+
+const AddPostForm = dynamic(() => import('@/components/posts/AddPostForm'), {
+  ssr: false,
+});
 
 export default function WritePostPage() {
   return (

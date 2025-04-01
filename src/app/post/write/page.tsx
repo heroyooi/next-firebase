41ae2423b7b8068517785bff1feae1664ab753be
heroyooi <heroyooi1018@gmail.com>
2025-04-01 12:35:53 +0900

    vercel 에러 해결

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -1,14 +1,14 @@
-import WriteClient from '@/components/posts/WriteClient';
+import { AddPostForm } from '@/components/posts';
 
-export const metadata = {
-  title: '글 작성 | Next.js + Firebase + GA',
-  description: 'Next.js + Firebase + GA 글 작성 페이지입니다.',
-};
+// export const metadata = {
+//   title: '글 작성 | Next.js + Firebase + GA',
+//   description: 'Next.js + Firebase + GA 글 작성 페이지입니다.',
+// };
 
 export default function WritePostPage() {
   return (
     <main>
-      <WriteClient />
+      <AddPostForm />
     </main>
   );
 }
--- src/components/posts/WriteClient.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import AddPostForm from '@/components/posts/AddPostForm';
-
-export default function WriteClient() {
-  return <AddPostForm />;
-}

88590f69f53558e6598bec09b251ad5a44b5c8bd
heroyooi <heroyooi1018@gmail.com>
2025-04-01 11:28:14 +0900

    vercel 배포를 위한 타입 해결

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,11 +3,14 @@ import { getPostById } from '@/lib/firestore';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-interface Props {
+interface PageProps {
   params: { id: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
   const post = await getPostById(params.id);
   return {
     title: `글 상세 | ${post?.title}` || '글 상세 | Next.js + Firebase + GA',
@@ -15,7 +18,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default async function PostDetailPage({ params }: Props) {
+export default async function PostDetailPage({ params }: PageProps) {
   const post = await getPostById(params.id);
   if (!post) return notFound();
 
